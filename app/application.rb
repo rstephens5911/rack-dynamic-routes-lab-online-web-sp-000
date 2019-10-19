c8db60182b81f3e97316559199c22fc9d1802877
Ronald Stephens <rstephens@bluemorpho.cc>
2019-10-19 17:32:29 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,4 +1,16 @@
 class Application
 
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+
+    if req.path == "/items/"
+      resp.write "#{item.name} #{ite.price}"
+    else
+      resp.write "Item not found"
+      resp.staus = 404
+    end
+  resp.finish
+end
 
 end
