de5a16e61a098edbc08660f82a1e67f89081f29c
Ronald Stephens <rstephens@bluemorpho.cc>
2019-10-19 18:01:49 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -3,7 +3,7 @@ class Application
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
-    
+
     if req.path != "/items"
       resp.write "Route not found"
       resp.status = 404
