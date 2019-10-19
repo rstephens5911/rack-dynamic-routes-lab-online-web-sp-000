435fc81a23448e85dbceca8040e20a497f472b8f
Ronald Stephens <rstephens@bluemorpho.cc>
2019-10-19 18:14:18 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -7,7 +7,7 @@ class Application
     if req.path.match (/items/)
       item_price = req.path.split("/items/").last
       item = @@items.find{ |i| i.price == item_price }
-      resp.write name.price
+      resp.write Item.name.price
 
     else resp.write "Route not found"
       resp.status = 404
