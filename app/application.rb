3ab54716c8ccf83e5709f3c1149bd825f0e19396
Ronald Stephens <rstephens@bluemorpho.cc>
2019-10-19 18:39:56 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -9,7 +9,7 @@ class Application
       item = @@items.find{ |i| i.name == item_name }
       resp.write item.price
       resp.status = 200
-        if !@@items.include?(item_name)
+    elsif !@@items.include?(item_name)
           resp.write "Item not found"
           resp.status = 400
         end
