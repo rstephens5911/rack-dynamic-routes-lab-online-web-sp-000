c003e384c2ca0acfa0804c78a1bb3b9acdaee177
Ronald Stephens <rstephens@bluemorpho.cc>
2019-10-19 17:33:58 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -5,7 +5,7 @@ class Application
     req = Rack::Request.new(env)
 
     if req.path == "/items/"
-      resp.write "#{item.name} #{item.price}"
+      resp.write "#{item.price}"
     else
       resp.write "Route not found"
       resp.status = 404
