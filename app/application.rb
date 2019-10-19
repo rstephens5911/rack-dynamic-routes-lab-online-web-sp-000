06971845c82353dbdd9e71ed0ff7d9c76918bb7f
Ronald Stephens <rstephens@bluemorpho.cc>
2019-10-19 17:50:05 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -4,12 +4,12 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
-    if req.path != "/items"
+    if req.path == "/items"
+      Items.all.find(item)
+      resp.write "#{items.price}"
+    elsif req.path != "/items"
       resp.write "Route not found"
       resp.status = 404
-    elsif req.path == "/items"
-      Item.all do |item|
-        resp.write "#{Item.all[item]}".first
       end
     end
   resp.finish
