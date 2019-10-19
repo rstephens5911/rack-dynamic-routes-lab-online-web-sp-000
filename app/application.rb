db9044f9f04ccf61531d833ca3d6fc2bb989f7d9
Ronald Stephens <rstephens@bluemorpho.cc>
2019-10-19 18:23:07 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -9,9 +9,7 @@ class Application
       item = @@items.find{ |i| i.name == item_name }
       resp.write item.price
       resp.status = 200
-    elsif !@@items.find{ |i| i.name == item_name }
-      resp.write "Item not found"
-      resp.status = 400
+  
 
     else resp.write "Route not found"
       resp.status = 404
